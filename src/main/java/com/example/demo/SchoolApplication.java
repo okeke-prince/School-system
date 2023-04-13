e6da0b2a553bc6103f213a41922f7299e6c9c078
Trikky <princeinme128@gmail.com>
2023-04-13 03:37:21 -0700

    update

--- src/main/java/com/example/demo/SchoolApplication.java
+++ src/main/java/com/example/demo/SchoolApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-public class DemoApplication {
+public class SchoolApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SchoolApplication.class, args);
 
 	}
 }
