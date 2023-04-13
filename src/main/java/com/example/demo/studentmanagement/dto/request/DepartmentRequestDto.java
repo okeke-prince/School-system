7b9ccfc2c35868266ee1caa8c936993a6fe11586
Trikky <princeinme128@gmail.com>
2023-04-13 03:31:30 -0700

    updates

--- src/main/java/com/example/demo/studentmanagement/dto/request/DepartmentRequestDto.java
+++ src/main/java/com/example/demo/studentmanagement/dto/request/DepartmentRequestDto.java
@@ -2,6 +2,8 @@ package com.example.demo.studentmanagement.dto.request;
 
 import com.example.demo.studentmanagement.entity.Course;
 import com.example.demo.studentmanagement.entity.Department;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +21,7 @@ public class DepartmentRequestDto {
     @NotNull(message = "name is required")
     private String name;
 
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private List<Course> courseList;
